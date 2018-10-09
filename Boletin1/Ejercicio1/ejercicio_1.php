461ea202757e69f9e357ab52f32a61988e745021
Alejandro <alesitonightray@gmail.com>
2018-10-09 16:49:40 +0200

    Boletin1-Ejercicio_1

---
+++ Boletin1/Ejercicio1/ejercicio_1.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>1</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+    <body>
+        <?php
+            include '../ejercicioPHP.php';
+        ?>
+        <table>
+            <tr>
+                <?php
+                    foreach ($arrayPersonas as $values) {
+                        foreach ($values as $key => $go) {
+                            echo "<td>";
+                                echo $key;
+                            echo "</td>";
+                        }
+                        break;
+                    }
+                ?>
+            </tr>
+            <?php
+
+                foreach ($arrayPersonas as $values) {
+                    echo "<tr>";
+                        foreach ($values as $go) {
+                            echo "<td>";
+                                echo $go."\n";
+                            echo "</td>";
+                        }
+                    echo "</tr>";
+                }
+            ?>
+        </table>
+
+    </body>
+</html>
