8b24460ae74c1efdd769dd2303b40e39482df520
Alejandro <alesitonightray@gmail.com>
2018-10-27 11:54:12 +0200

    Boletin 3

---
+++ Boletin3EnCADENADO/Ejercicio01/ejercicio-1.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Transformacion</title>
+  </head>
+  <body>
+    <?php
+      $cadena = 'En un lugar de la mancha de cuyo nombre';
+
+      
+    ?>
+  </body>
+</html>
---
+++ Boletin3EnCADENADO/funciones.php
@@ -0,0 +1,8 @@
+<?php
+  function convertirMayusculas($cadena) {
+
+  }
+  function convertirMinusculas($cadena) {
+
+  }
+ ?>
